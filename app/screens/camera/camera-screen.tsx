fae9de247e57f0364cc3e91cd15fb4f4b1553595
Dmytro Klymenko <klimenkodr@gmail.com>
2022-04-26 13:16:17 +0300

    add camera screen

--- app/screens/camera/camera-screen.tsx
+++ app/screens/camera/camera-screen.tsx
@@ -1,24 +1,68 @@
-import React, { FC } from "react"
+import React, { FC, useCallback, useEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
+import { Linking, ViewStyle } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "../../navigators"
 import { Screen, Text } from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 import { color } from "../../theme"
+import { Camera, CameraPermissionStatus, useCameraDevices } from "react-native-vision-camera"
 
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
   flex: 1,
 }
+const FULL: ViewStyle = { flex: 1 }
 
 export const CameraScreen: FC<StackScreenProps<NavigatorParamList, "camera">> = observer(
   function CameraScreen() {
-    // Pull in navigation via hook
-    // const navigation = useNavigation()
+    const [cameraPermission, setCameraPermission] = useState<CameraPermissionStatus>()
+    const [microphonePermission, setMicrophonePermission] = useState<CameraPermissionStatus>()
+
+    useEffect(() => {
+      Camera.getCameraPermissionStatus().then(setCameraPermission)
+      Camera.getMicrophonePermissionStatus().then(setMicrophonePermission)
+    }, [])
+
+    const requestMicrophonePermission = useCallback(async () => {
+      console.log("Requesting microphone permission...")
+      const permission = await Camera.requestMicrophonePermission()
+      console.log(`Microphone permission status: ${permission}`)
+
+      if (permission === "denied") await Linking.openSettings()
+      setMicrophonePermission(permission)
+    }, [])
+
+    const requestCameraPermission = useCallback(async () => {
+      console.log("Requesting camera permission...")
+      const permission = await Camera.requestCameraPermission()
+      console.log(`Camera permission status: ${permission}`)
+
+      if (permission === "denied") await Linking.openSettings()
+      setCameraPermission(permission)
+    }, [])
+
+    const devices = useCameraDevices()
+    const device = devices.back
     return (
       <Screen style={ROOT} preset="scroll">
         <Text preset="header" text="camera" />
+        {cameraPermission && cameraPermission !== "authorized" && (
+          <Text>
+            Vision Camera needs <Text>Camera permission</Text>.
+            <Text onPress={requestCameraPermission}>Grant</Text>
+          </Text>
+        )}
+        {microphonePermission && microphonePermission !== "authorized" && (
+          <Text>
+            Vision Camera needs <Text>Microphone permission</Text>.
+            <Text onPress={requestMicrophonePermission}>Grant</Text>
+          </Text>
+        )}
+
+        {cameraPermission === "authorized" && microphonePermission === "authorized" && device && (
+          <Camera style={FULL} device={device} isActive={true} />
+        )}
       </Screen>
     )
   },
