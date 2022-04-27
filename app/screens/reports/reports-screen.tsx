ef4f129044cba9b8df030b0d4e342c44d663c836
Dmytro Klymenko <klimenkodr@gmail.com>
2022-04-27 22:33:57 +0300

    print reports

--- app/screens/reports/reports-screen.tsx
+++ app/screens/reports/reports-screen.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
+import { ViewStyle, Image, View } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "../../navigators"
 import { Screen, Text } from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../theme"
+import { useStores } from "../../models"
 
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
@@ -16,13 +17,24 @@ const ROOT: ViewStyle = {
 export const ReportsScreen: FC<StackScreenProps<NavigatorParamList, "reports">> = observer(
   function ReportsScreen() {
     // Pull in one of our MST stores
-    // const { someStore, anotherStore } = useStores()
+    const { reportStore } = useStores()
+    const { reports } = reportStore
 
     // Pull in navigation via hook
     // const navigation = useNavigation()
     return (
       <Screen style={ROOT} preset="scroll">
         <Text preset="header" text="reports" />
+        {reports.map((report) => (
+          <View key={report.id}>
+            <Image
+              style={{ width: 100, height: 100 }}
+              key={report.id}
+              source={{ uri: `file://${report.photo}` }}
+            />
+            <Text>{report.status}</Text>
+          </View>
+        ))}
       </Screen>
     )
   },
