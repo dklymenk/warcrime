5ba221ff9836c3901cdee806a8f5a32c7714416f
Dmytro Klymenko <klimenkodr@gmail.com>
2022-05-19 09:52:32 +0300

    allow camera video recording

--- app/screens/camera/camera-screen.tsx
+++ app/screens/camera/camera-screen.tsx
@@ -62,20 +62,34 @@ const BUTTON = (orientation: "PORTRAIT" | "LANDSCAPE"): ViewStyle => ({
   // transform: [{translateY: }],
   alignSelf: orientation === "PORTRAIT" ? "center" : "flex-end",
 })
+const VIDEO_BUTTON = (orientation: "PORTRAIT" | "LANDSCAPE", isRecording: boolean): ViewStyle => ({
+  position: "absolute",
+  width: 48,
+  height: 48,
+  borderRadius: 50,
+  borderWidth: 4,
+  borderColor: color.palette.deepPurple,
+  backgroundColor: isRecording ? color.palette.angry : color.palette.lightGrey,
+  bottom: orientation === "PORTRAIT" ? 8 : Dimensions.get("screen").height / 2 + 42,
+  right: orientation === "LANDSCAPE" ? 8 : Dimensions.get("screen").width / 2 - 64 - 24,
+  // transform: [{translateY: }],
+  alignSelf: orientation === "PORTRAIT" ? "center" : "flex-end",
+})
 
 type ValueOf<T> = T[keyof T]
 
 export const CameraScreen: FC<StackScreenProps<NavigatorParamList, "camera">> = observer(
   function CameraScreen() {
     const [cameraPermission, setCameraPermission] = useState<CameraPermissionStatus>()
-    // const [microphonePermission, setMicrophonePermission] = useState<CameraPermissionStatus>()
+    const [microphonePermission, setMicrophonePermission] = useState<CameraPermissionStatus>()
     const [locationPermission, setLocationPermission] = useState<ValueOf<typeof RESULTS>>()
     const [galleryWritePermission, setGalleryWritePermission] = useState<ValueOf<typeof RESULTS>>()
     const [position, setPosition] = useState<Geolocation.GeoPosition>()
+    const [isRecording, setIsRecording] = useState<boolean>()
 
     useEffect(() => {
       Camera.getCameraPermissionStatus().then(setCameraPermission)
-      // Camera.getMicrophonePermissionStatus().then(setMicrophonePermission)
+      Camera.getMicrophonePermissionStatus().then(setMicrophonePermission)
       check(
         Platform.OS === "android"
           ? PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
@@ -93,14 +107,14 @@ export const CameraScreen: FC<StackScreenProps<NavigatorParamList, "camera">> = 
       })
     }, [])
 
-    // const requestMicrophonePermission = useCallback(async () => {
-    //   // console.log("Requesting microphone permission...")
-    //   const permission = await Camera.requestMicrophonePermission()
-    //   // console.log(`Microphone permission status: ${permission}`)
+    const requestMicrophonePermission = useCallback(async () => {
+      // console.log("Requesting microphone permission...")
+      const permission = await Camera.requestMicrophonePermission()
+      // console.log(`Microphone permission status: ${permission}`)
 
-    //   if (permission === "denied") await Linking.openSettings()
-    //   setMicrophonePermission(permission)
-    // }, [])
+      if (permission === "denied") await Linking.openSettings()
+      setMicrophonePermission(permission)
+    }, [])
 
     const requestCameraPermission = useCallback(async () => {
       // console.log("Requesting camera permission...")
@@ -184,18 +198,59 @@ export const CameraScreen: FC<StackScreenProps<NavigatorParamList, "camera">> = 
       toast("cameraScreen.photoTaken")
       CameraRoll.save(photo.path)
     }
+    const startRecording = async () => {
+      setIsRecording(true)
+      const positionAtVideoStart = { ...position }
+      camera.current.startRecording({
+        onRecordingFinished: async (video) => {
+          setIsRecording(false)
+          const id = uuidv4()
+          const uri = `${FileSystem.documentDirectory}${id}.mp4`
+          await FileSystem.copyAsync({
+            from: `file://${video.path}`,
+            to: uri,
+          })
+          reportStore.addReport({
+            id,
+            photo: `${id}.mp4`,
+            status: ReportStatus.Pending,
+            latLong: positionAtVideoStart
+              ? `${positionAtVideoStart.coords.latitude},${positionAtVideoStart.coords.longitude}`
+              : undefined,
+          })
+          toast("cameraScreen.photoTaken")
+          CameraRoll.save(video.path)
+        },
+        onRecordingError: (error) => {
+          console.tron.error(error, null)
+          toast("cameraScreen.videoError")
+          setIsRecording(false)
+        },
+      })
+    }
+    const stopRecording = async () => {
+      await camera.current.stopRecording()
+    }
 
     const orientation = useOrientation()
 
     return (
       <Screen style={ROOT} preset="scroll">
         {cameraPermission === "authorized" &&
-        // microphonePermission === "authorized" &&
+        microphonePermission === "authorized" &&
         galleryWritePermission === RESULTS.GRANTED &&
         locationPermission === RESULTS.GRANTED &&
         device ? (
           <View style={CAMERA_CONTAINER}>
-            <Camera ref={camera} style={FULL} device={device} isActive={true} photo={true} />
+            <Camera
+              ref={camera}
+              style={FULL}
+              device={device}
+              isActive={true}
+              photo={true}
+              video={true}
+              audio={true}
+            />
             <Text style={[POSITION, position && POSITION_FOUND]}>
               {position
                 ? `${position.coords.latitude}, ${position.coords.longitude}`
@@ -203,6 +258,10 @@ export const CameraScreen: FC<StackScreenProps<NavigatorParamList, "camera">> = 
             </Text>
             <Header style={HEADER} leftIcon="back" headerTx="mainMenuScreen.camera" />
             <Button style={BUTTON(orientation)} onPress={takePicture} />
+            <Button
+              style={VIDEO_BUTTON(orientation, isRecording)}
+              onPress={isRecording ? stopRecording : startRecording}
+            />
           </View>
         ) : (
           <Header leftIcon="back" headerTx="mainMenuScreen.camera" />
@@ -214,12 +273,12 @@ export const CameraScreen: FC<StackScreenProps<NavigatorParamList, "camera">> = 
               <Button tx={"cameraScreen.grant"} onPress={requestCameraPermission} />
             </View>
           )}
-          {/* {microphonePermission && microphonePermission !== "authorized" && ( */}
-          {/*   <> */}
-          {/*     <Text tx={"cameraScreen.microphonePermissionRequired"} /> */}
-          {/*     <Button tx={"cameraScreen.grant"} onPress={requestMicrophonePermission} /> */}
-          {/*   </> */}
-          {/* )} */}
+          {microphonePermission && microphonePermission !== "authorized" && (
+            <>
+              <Text tx={"cameraScreen.microphonePermissionRequired"} />
+              <Button tx={"cameraScreen.grant"} onPress={requestMicrophonePermission} />
+            </>
+          )}
           {locationPermission && locationPermission !== RESULTS.GRANTED && (
             <View style={PERMISSION_CONTAINER}>
               <Text style={PERMISSION_TEXT} tx={"cameraScreen.locationPermissionRequired"} />
