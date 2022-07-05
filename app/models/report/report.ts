7b38198fb0422929260c89f140c3cf4bb2c98434
Dmytro Klymenko <klimenkodr@gmail.com>
2022-07-06 00:06:37 +0300

    use raw uploader on iOS

--- app/models/report/report.ts
+++ app/models/report/report.ts
@@ -3,6 +3,7 @@ import { withEnvironment } from "../extensions/with-environment"
 import { RootStore } from "../root-store/root-store"
 import { v4 as uuidv4 } from "uuid"
 import "react-native-get-random-values"
+import { Platform } from "expo-modules-core"
 
 export enum ReportStatus {
   Pending = "PENDING",
@@ -38,7 +39,11 @@ export const ReportModel = types
       self.loading = true
       const rootStore = getRoot<RootStore>(self)
       try {
-        const uploadFileResult = yield* toGenerator(self.environment.api.uploadFile(self.photo))
+        const uploadFileResult = yield* toGenerator(
+          Platform.OS === "ios"
+            ? self.environment.api.uploadRawFile(self.photo)
+            : self.environment.api.uploadFile(self.photo),
+        )
 
         if (uploadFileResult.kind === "ok") {
           const result = yield* toGenerator(
