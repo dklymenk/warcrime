f7f37e904299f8ca6e0699e2fb6b507a9d0b1561
Dmytro Klymenko <klimenkodr@gmail.com>
2022-07-15 17:42:34 +0300

    show outdated location warning

--- app/components/location/location.tsx
+++ app/components/location/location.tsx
@@ -1,6 +1,9 @@
 import * as React from "react"
 import { StyleProp, TextStyle, View, ViewStyle } from "react-native"
 import { observer } from "mobx-react-lite"
+import { formatDistance } from "date-fns"
+import { uk } from "date-fns/locale"
+import differenceInMinutes from "date-fns/differenceInMinutes"
 import { color } from "../../theme"
 import { Text } from "../text/text"
 import { LocationSnapshot } from "../../models/location/location"
@@ -18,6 +21,13 @@ const TEXT_FOUND: TextStyle = {
   color: color.palette.offWhite,
 }
 
+const SMALL_TEXT: TextStyle = {
+  color: color.palette.offWhite,
+  fontSize: 12,
+  lineHeight: 12,
+  textAlign: "right",
+}
+
 export interface LocationProps {
   /**
    * An optional style override useful for padding & margin.
@@ -33,11 +43,36 @@ export const Location = observer(function Location(props: LocationProps) {
   const { style, location } = props
   const styles = Object.assign({}, CONTAINER, style)
 
+  const [distance, setDistance] = React.useState("")
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      const diffirenceInMinutes = differenceInMinutes(new Date(), new Date(location.updatedAt))
+      if (diffirenceInMinutes < 1) {
+        return
+      }
+      const distance = formatDistance(new Date(location.updatedAt), new Date(), {
+        addSuffix: true,
+        locale: uk,
+        includeSeconds: true,
+      })
+      setDistance(distance)
+    }, 5 * 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  React.useEffect(() => {
+    setDistance("")
+  }, [location.latLong])
+
   return (
     <View style={styles}>
       <Text style={[TEXT, location.latLong && TEXT_FOUND]}>
         {location.latLong || translate("cameraScreen.waitingForLocation")}
       </Text>
+      <Text style={SMALL_TEXT}>{distance}</Text>
     </View>
   )
 })
